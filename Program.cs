efef1ac82e68a50ea5aac2b5b08401510dbeac12
F.Buse Özmen <F.Buse zmen@MSI>
2022-01-04 01:09:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do not repeat yourself - Kendini tekrarlama
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış oku");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış oku");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi oku");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
